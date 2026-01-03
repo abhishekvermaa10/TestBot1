06c864530e2bb5b7ce929a58a1fa5a69a3df325e
Abhishek Verma <abhishekvermaa10@gmail.com>
2026-01-03 13:04:28 +0530

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,10 +10,15 @@ public class App {
 	
 	public static void main(String[] args) {
 		try {
+			int port = Integer.parseInt(
+                    System.getenv().getOrDefault("PORT", "8080")
+            );
+            HealthServer.start(port);
 			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 			botsApi.registerBot(new Bot());
 			log.info("Bot started successfully!");
-		} catch (TelegramApiException e) {
+			Thread.currentThread().join();
+		} catch (Exception e) {
 			log.error("Failed to start bot", e);
 		}
 	}
---
+++ src/main/java/org/example/HealthServer.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HealthServer {
+
+	public static void start(int port) {
+		try {
+			HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+			server.createContext("/", exchange -> {
+				String response = "OK";
+				exchange.sendResponseHeaders(200, response.length());
+				try (OutputStream os = exchange.getResponseBody()) {
+					os.write(response.getBytes());
+				}
+			});
+			server.start();
+			log.info("Health server started on port {}", port);
+
+		} catch (Exception e) {
+			log.error("Failed to start health server", e);
+		}
+	}
+	
+}
